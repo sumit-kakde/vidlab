5bbed178aa41864e821badbe1c94fc31d47d69fd
sumit-kakde <sbkakde11@gmail.com>
2025-09-20 09:17:23 +0530

    removed minor mistakes

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -10,10 +10,10 @@ class ApiError extends Error{
         this.data = null
         this.message = message
         this.success = false
-        this .errors = errors
+        this.errors = errors
 
-        if(statck){
-            this.stack = statck
+        if(stack){
+            this.stack = stack
         }else{
             Error.captureStackTrace(this,thi.constructor)
         }
